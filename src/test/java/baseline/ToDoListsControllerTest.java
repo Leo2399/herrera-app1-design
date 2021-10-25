8a8fee517f4f9ab40e0377ac5d3c565ae9fa17f7
Leonardo Herrera <leo2399@gmail.com>
2021-10-24 20:27:47 -0400

    test cases added

---
+++ src/test/java/baseline/ToDoListsControllerTest.java
@@ -0,0 +1,28 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ToDoListsControllerTest {
+
+    @Test
+    void addTask() {
+        // compare a set of strings with a title, a due date, and a due date and check if they match
+    }
+
+    @Test
+    void deleteTask() {
+        // same as the add task method test, make sure the set of strings match 
+    }
+
+    @Test
+    void saveToFile() {
+        // test to make sure that a file is opened to write into
+    }
+
+    @Test
+    void initialize() {
+        // not sure how it will be tested just yet
+    }
+}
